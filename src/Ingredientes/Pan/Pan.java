e8b079d0326d4073a72b4de9910cfbb203652850
Edgar Leon <edgar.leon@alumnos.uneatlantico.es>
2024-06-13 12:25:50 +0200

    Agregar Panes

--- src/Ingredientes/Pan/Pan.java
+++ src/Ingredientes/Pan/Pan.java
@@ -1,2 +1,15 @@
-public abstract class Pan {
+public class Pan
+{
+
+    public PanBrioche(String nuevoNombre)
+    {
+        nombre = "Brioche";
+        vista = "##############"
+    }
+
+    public PanIntegral(String nuevoNombre)
+    {
+        nombre = "Integral";
+        vista = "||||||||||||||"
+    }
 }
