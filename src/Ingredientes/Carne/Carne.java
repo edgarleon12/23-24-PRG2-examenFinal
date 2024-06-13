2030746b39ad727bf2a9204a1f54af20e425a3b8
root <edgar.leon@alumnos.uneatlantico.es>
2024-06-13 12:33:51 +0200

    Agregar Tipos de Panes y TIpos de cocciones

--- src/Ingredientes/Carne/Carne.java
+++ src/Ingredientes/Carne/Carne.java
@@ -1,2 +1,19 @@
-public abstract class Carne {
+import CarneCoccion;
+
+public class Carne
+{
+
+    public Res(String nuevaCarne)
+    {
+        nombre = "Res";
+        vista = "::::::::::::"
+        CarneCoccion.tipo()
+    }
+
+    public Pollo(String nuevaCarne)
+    {
+        nombre = "Pollo";
+        vista = ";;;;;;;;;;;;"
+        CarneCoccion.tipo()
+    }
 }
--- src/Ingredientes/Carne/Coccion/CarneCoccion.java
+++ src/Ingredientes/Carne/Coccion/CarneCoccion.java
@@ -1,2 +1,21 @@
-public abstract class CarneCoccion {
+public class CarneCoccion
+{
+    private String tipo;
+    public Pocohecha(String nuevoTipo)
+    {
+        tipo = "Poco Hecha";
+        return tipo
+    }
+
+    public MedioHecha(String nuevoTipo)
+    {
+        tipo = "MedioHecha";
+        return tipo
+    }
+
+    public MuyHecha(String nuevoTIpo)
+    {
+        tipo = "MuyHecha";
+        return tipo
+    }
 }
